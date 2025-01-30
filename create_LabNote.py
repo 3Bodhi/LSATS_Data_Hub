b29e2cdd157a5b495a6a130853c97454ed7256e9
MatthewYodhes <myodhes@umich.edu>
2025-01-30 11:53:25 -0500

    Begin compliance ticket automator (example tdx post + psuedo code)

--- create_LabNote.py
+++ create_LabNote.py
@@ -10,12 +10,12 @@ TDX_APP_ID = os.getenv('TDX_APP_ID')
 API_TOKEN = os.getenv('TDX_API_TOKEN')
 
 tdx_service = TeamDynamixFacade(TDX_BASE_URL, TDX_APP_ID, API_TOKEN)
-lab = tdx_service.create_lab('danweiss')
+lab = tdx_service.create_lab('tarazlee')
 #tickets = tdx_service.get_user_tickets_by_uniqname('danweiss')
 #print(tickets[0])
 '''
 for ticket in tickets:
     print(ticket['RequestorLastName'],ticket['Title'],ticket['StatusName'])
 '''
-tdx_service.tickets.add_ticket_configuration_item(7151528,1205942)
+#tdx_service.tickets.add_ticket_configuration_item(7151528,1205942)
 #print(tdx_service.configuration_items.get_ci(1205942))
--- example_tdx_sheet_post.py
+++ example_tdx_sheet_post.py
@@ -23,12 +23,14 @@ tdx_service = TeamDynamixFacade(TDX_BASE_URL, TDX_APP_ID, API_TOKEN)
 adapter = GoogleSheetsAdapter(CREDENTIALS_FILE)
 sheet = Sheet(adapter, SPREADSHEET_ID, SHEET_NAME, header_row=1)
 
-
-ticket = tdx_service.tickets.get_ticket(7085894)
+ticket_id = 6229435
+ticket = tdx_service.tickets.get_ticket(ticket_id)
 ticket_email = ticket['RequestorEmail']
 column_names = sheet.get_column_names()
-
+print(column_names)
+print(ticket_email)
 search = sheet.search_columns(ticket_email,columns=['Owner Email'])
+print(search)
 results = pd.DataFrame(search[:-1],columns=column_names)
 row_numbers = search[-1]
 url = sheet.generate_url
@@ -38,10 +40,11 @@ results_str = results.to_string()
 prompt = '''You will be provided with a table related to computers
 which need some sort of fix. Create a friendly email from a tech
 to a user noting which computers have issues, their serial numbers
-and the fix required. Only return the email'''
+and the fix required. Only return the email. My name is Matthew Yodhes. Our team name is LSA Technology Services'''
 
 completion = client.chat.completions.create(
-  model="llama3.1-70b",
+  #model="llama3.1-70b",
+  model = 'deepseek-r1-32b?',
   messages=[
       {"role": "system", "content": prompt },
     {"role": "user", "content": results_str }
@@ -52,4 +55,4 @@ completion = client.chat.completions.create(
 comment += str(completion.choices[0].message.content) + "\n\n"
 for index, row in enumerate(row_numbers):
     comment += f"{results['Computer Name'].iloc[index]} - {sheet.generate_url(row)}" + "\n\n"
-tdx_service.tickets.update_ticket(7085894,comments=comment,private=True,commrecord=False, rich=False)
+tdx_service.tickets.update_ticket(ticket_id,comments=comment,private=True,commrecord=False, rich=False)
