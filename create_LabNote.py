cbb67f5b63aaaab4df1bca00c9c36dd117f6cb35
MatthewYodhes <myodhes@umich.edu>
2025-03-20 10:57:06 -0400

    create main function to make cli executable

--- create_LabNote.py
+++ create_LabNote.py
@@ -1,21 +1,30 @@
 import dotenv
+import argparse
 from google_drive import GoogleSheetsAdapter, Sheet
 from teamdynamix import TeamDynamixFacade
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
-TDX_BASE_URL = os.getenv('TDX_BASE_URL')
-TDX_APP_ID = os.getenv('TDX_APP_ID')
-API_TOKEN = os.getenv('TDX_API_TOKEN')
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Create a lab note for a PI in TeamDynamix')
+    parser.add_argument('uniqname', help='Uniqname of the PI')
+    parser.add_argument('--env-file', default='.env', help='Path to the .env file')
+    args = parser.parse_args()
 
-tdx_service = TeamDynamixFacade(TDX_BASE_URL, TDX_APP_ID, API_TOKEN)
-lab = tdx_service.create_lab('tarazlee')
-#tickets = tdx_service.get_user_tickets_by_uniqname('danweiss')
-#print(tickets[0])
-'''
-for ticket in tickets:
-    print(ticket['RequestorLastName'],ticket['Title'],ticket['StatusName'])
-'''
-#tdx_service.tickets.add_ticket_configuration_item(7151528,1205942)
-#print(tdx_service.configuration_items.get_ci(1205942))
+    # Load environment variables
+    load_dotenv(args.env_file)
+
+    TDX_BASE_URL = os.getenv('TDX_BASE_URL')
+    TDX_APP_ID = os.getenv('TDX_APP_ID')
+    API_TOKEN = os.getenv('TDX_API_TOKEN')
+
+    # Initialize TeamDynamix service
+    tdx_service = TeamDynamixFacade(TDX_BASE_URL, TDX_APP_ID, API_TOKEN)
+
+    # Create the lab
+    lab = tdx_service.create_lab(args.uniqname)
+    print(f"Created lab for {args.uniqname}: {lab['Name']} (ID: {lab['ID']})")
+
+if __name__ == '__main__':
+    main()
