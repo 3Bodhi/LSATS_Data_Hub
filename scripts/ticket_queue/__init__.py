4660c04b3fa0074a504d5f77f7458bba4f0e3cf0
MatthewYodhes <myodhes@umich.edu>
2025-12-16 15:24:42 -0500

    queu to ticket queue folder rename to avoid collision

---
+++ scripts/ticket_queue/__init__.py
@@ -0,0 +1,6 @@
+"""
+Queue daemon package for processing TeamDynamix ticket reports.
+
+This package provides a robust daemon service for monitoring TDX reports
+and performing idempotent actions on tickets.
+"""
